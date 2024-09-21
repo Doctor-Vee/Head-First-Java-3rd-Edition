de7142131127646d26b97ff007ddd7a545244512
DoctorVee <vchinewubeze@reloadly.com>
2024-09-21 20:56:35 +0100

    :sparkles:

--- src/MyFiles/ch6/startup_game/StartupBust.java
+++ src/MyFiles/ch6/startup_game/StartupBust.java
@@ -17,32 +17,44 @@ public class StartupBust {
         System.out.println("This is a game where you can shoot down startups by guessing their locations... Are you ready?? 🥳");
     }
 
-    void startPlaying(){
-
-    }
-
-
-    public static void main(String[] args) {
-
-
+    void startPlaying() {
         int liveStartups = 3;
 
         do {
             String guess = helper.takeUserGuess("Enter a cell from A0 to G6: ");
             numOfGuesses++;
-            String result0 = startups[0].checkYourself(guess);
-            String result1 = startups[1].checkYourself(guess);
-            String result2 = startups[2].checkYourself(guess);
 
-            if (result0.equals("kill") || result1.equals("kill") || result2.equals("kill")) {
+
+            String [] results = checkUserGuess(guess);
+
+            if (results[0].equals("kill") || results[1].equals("kill") || results[2].equals("kill")) {
                 System.out.println("kill");
                 liveStartups--;
-            } else if (result0.equals("hit") || result1.equals("hit") || result2.equals("hit")) {
+            } else if (results[0].equals("hit") || results[1].equals("hit") || results[2].equals("hit")) {
                 System.out.println("hit");
             } else {
                 System.out.println("miss");
             }
         } while (liveStartups != 0);
+    }
+
+    private String[] checkUserGuess(String guess) {
+        String result0 = startups[0].checkYourself(guess);
+        String result1 = startups[1].checkYourself(guess);
+        String result2 = startups[2].checkYourself(guess);
+        return new String[]{result0, result1, result2};
+    }
+
+    void finishGame() {
         System.out.println("You used " + numOfGuesses + " guesses");
+    }
+
+
+    public static void main(String[] args) {
+        StartupBust game = new StartupBust();
+        game.setUpGame();
+        game.startPlaying();
+        game.finishGame();
+
     }
 }
