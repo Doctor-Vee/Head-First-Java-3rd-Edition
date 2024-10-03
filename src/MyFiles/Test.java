c428f94822ad66faa30fc27a123fd8602e6854fd
DoctorVee <vchinewubeze@reloadly.com>
2024-10-03 17:34:22 +0100

    :sparkles:

--- src/MyFiles/Test.java
+++ src/MyFiles/Test.java
@@ -2,14 +2,27 @@ package MyFiles;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
+
+        int boy = 54;
+        if(boy < 50 && (boy = 50) > 70){
+            System.out.println("Boy is big and his value is " + boy);
+        } else {
+            System.out.println("Boy is small with a value of " + boy);
+        }
 
         char name = 65;
         System.out.println((char) (name));
 
         String [] names = new String[]{"John", "Mary", "Israel"};
+        System.out.println("Names: ---------");
+        System.out.println(Arrays.toString(names));
+        Arrays.sort(names);
+        System.out.println("Sorted Names: --------------");
+        System.out.println(Arrays.toString(names));
         String [] students = names.clone();
         System.out.println(Arrays.toString(students));
 
@@ -22,6 +35,7 @@ public class Test {
         list.add(2, 50);
         list.add(3, 36);
         System.out.println("Address of list after adding 4 elements = " + System.identityHashCode(list));
+        System.out.println(list.indexOf(500)); // -1
         list.add(4, 42);
         list.add(16);
         list.add(6, 110);
@@ -64,7 +78,38 @@ public class Test {
 
         System.out.println("Hello world" + $);
 
+        List<String> women = new ArrayList<>(Arrays.asList("Mary", "Angel", "Martha", "Esther", "Deborah", "Esther", "Mary", "Martha", "Mary"));
+        List<String> mWomen = List.of("Mary", "Martha");
+        System.out.println("Women = " + women);
+        women.removeAll(mWomen);
+        System.out.println("Women again = " + women);
+
+        int [] arr = new int[] {1,2,3,4,5,6,66,77,88,99,120};
+        System.out.println(binarySearch(arr, 0, 10, 16));
+
     }
+
+    static int binarySearch(int[] arr, int l, int r, int x) {
+        System.out.println("Entered the function");
+        if (r >= l) {
+            int mid = l + (r - l) / 2;  // line 4
+
+            // If the element is present at the middle
+            if (arr[mid] == x)  // line 8
+                return mid;
+
+            // If element is smaller than mid, it must be in left subarray
+            if (arr[mid] > x)  // line 10
+                return binarySearch(arr, l, mid - 1, x);
+
+            // Else the element must be in the right subarray
+            return binarySearch(arr, mid + 1, r, x);
+        }
+
+        // Element is not present in the array
+        return -1;
+    }
+
 
     static int fibonacci(int n) {
         if (n <= 1) return n;
